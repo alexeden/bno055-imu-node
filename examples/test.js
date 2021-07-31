ba7c96189b2b926e2eea1d15ab0a9e2e17a731d5
Simon Colmer <simon@colmer.me>
2021-07-31 18:50:32 +0100

    Restore original test.js

--- examples/test.js
+++ examples/test.js
@@ -1,53 +1,46 @@
 const { AxisSign, Axis, BNO055, OpMode, DeviceAddress } = require('../dist');
-const fs = require('fs');
-
-const offsetsPath = "./offsets.json";
-const wait = (t) => new Promise(ok => setTimeout(ok, t));
 
 (async () => {
   try {
-    const imu = await BNO055.begin(DeviceAddress.A, OpMode.FullFusion, 3);
+    const imu = await BNO055.begin(DeviceAddress.A, OpMode.FullFusion, 1);
     await imu.resetSystem();
+    // await imu.setAxisMapping({
+    //   X: {
+    //     axis: Axis.X,
+    //     sign: AxisSign.Positive,
+    //   },
+    //   Y: {
+    //     axis: Axis.Z,
+    //     sign: AxisSign.Negative,
+    //   },
+    //   Z: {
+    //     axis: Axis.Y,
+    //     sign: AxisSign.Positive,
+    //   },
+    // });
 
-    if(fs.existsSync(offsetsPath)) {
-      console.log("Reading offsets from disk,", offsetsPath);
-      const data = fs.readFileSync(offsetsPath, {encoding: 'utf8', flag: 'r'});
+    const printEverything = async () => {
 
-      const data2 = JSON.parse(data.toString());
-      console.log(data2);
-      
-      console.log("Running setSensorOffsets....");
-      await imu.setSensorOffsets(data2);
-      console.log("Done?!");
-    }
+      console.log('current mode: ', await imu.getMode());
+      console.log('current page: ', await imu.getPage());
+      console.log('system status: ', await imu.getSystemStatus());
+      console.log('system error: ', await imu.getSystemError());
+      console.log('temp: ', await imu.getTemperature());
+      console.log('self-test results: ', await imu.getSelfTestResults());
 
-    let calibrated = false;
-    while(!calibrated) {
-      await wait(3333);
-      console.log('calibration: ', await imu.getCalibrationStatuses());
-
-      calibrated = await imu.isFullyCalibrated();
-      console.log('is calibrated: ', calibrated);
-
-      const offsets = await imu.getSensorOffsets();
-      console.log('offsets: ', offsets);
-
-      if(calibrated) {
-        console.log("Storing offsets to disk", offsetsPath, offsets);
-        const data = JSON.stringify(offsets);
-        fs.writeFileSync(offsetsPath, data);
-      }
-    }
-
-    while(true) {
       console.log('axis mapping: ', await imu.getAxisMapping());
+      console.log('versions: ', await imu.getVersions());
       console.log('units: ', await imu.getUnits());
       console.log('euler: ', await imu.getEuler());
       console.log('quat: ', await imu.getQuat());
-      console.log('--------------------------------');
+      console.log('calibration: ', await imu.getCalibrationStatuses());
+      console.log('is calibrated: ', await imu.isFullyCalibrated());
+      console.log('offsets: ', await imu.getSensorOffsets());
+      setTimeout(printEverything, 3333);
+    };
 
-      await wait(3333);
-    }
+    await printEverything();
+
   }
   catch (error) {
     console.error('error: ', error);
